0d9671713d5465fc05c7bf02ac1586cdfac78b7b
CareforYourCandy <lucianopinedo@gmail.com>
2018-05-19 17:21:47 -0400

    Diferenciacion de usuarios y almacenamiento de su info

--- app/src/main/java/com/example/virginia/panadelivery/RegisterActivity.java
+++ app/src/main/java/com/example/virginia/panadelivery/RegisterActivity.java
@@ -6,6 +6,7 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,11 +14,17 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
-public class RegisterActivity extends AppCompatActivity implements View.OnClickListener{
+import java.util.HashMap;
+import java.util.Map;
+
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button buttonRegister;
     private EditText editTextEmail;
@@ -25,7 +32,7 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
     private TextView textViewSignIn;
     private ProgressDialog progressDialog;
     private FirebaseAuth firebaseauth;
-
+    private FirebaseFirestore firestore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +40,7 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
         setContentView(R.layout.activity_register);
 
         firebaseauth = FirebaseAuth.getInstance();
+        firestore = FirebaseFirestore.getInstance();
         progressDialog = new ProgressDialog(this);
 
         buttonRegister = (Button) findViewById(R.id.buttonRegister);
@@ -81,12 +89,33 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
                             Log.d(TAG, "createUserWithEmail:success");
                             FirebaseUser user = mAuth.getCurrentUser();
                             updateUI(user);*/
+
+                            Map<String, Object> usuario = new HashMap<>();
+                            //Colocar resto de la info
+                            usuario.put("email", editTextEmail.getText().toString().trim());
+                            usuario.put("rol", 1);
+
+
+                            firestore.collection("Usuarios").document(editTextEmail.getText().toString().trim()).set(usuario)
+                                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                        @Override
+                                        public void onSuccess(Void aVoid) {
+                                            Log.d("FIREBASE", "DocumentSnapshot successfully written!");
+                                        }
+                                    })
+                                    .addOnFailureListener(new OnFailureListener() {
+                                        @Override
+                                        public void onFailure(@NonNull Exception e) {
+                                            Log.w("FIREBASE", "Error writing document", e);
+                                        }
+                                    });
+
                             progressDialog.cancel();
                             Toast.makeText(RegisterActivity.this, "Registro exitoso!", Toast.LENGTH_SHORT).show();
 
                         } else {
                             // If sign in fails, display a message to the user.
-                            //Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Log.w("ERROR REGISTRO", "createUserWithEmail:failure", task.getException());
                             progressDialog.cancel();
                             Toast.makeText(RegisterActivity.this, "Fallo en el registro, intente de nuevo.", Toast.LENGTH_SHORT).show();
                             //updateUI(null);
